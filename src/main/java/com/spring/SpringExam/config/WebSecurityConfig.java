cfca37e139096ef3e0ac75857637618d8bd4e62a
Farid <fabdullayev902@gmail.com>
2021-11-01 19:10:09 +0400

    make it better

--- src/main/java/com/spring/SpringExam/config/WebSecurityConfig.java
+++ src/main/java/com/spring/SpringExam/config/WebSecurityConfig.java
@@ -46,7 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
 
         // The pages does not require login
-        http.authorizeRequests().antMatchers("/", "/login","/about","/registration", "/logout").permitAll();
+        http.authorizeRequests().antMatchers("/", "/login","/about","/registration", "/logout","/admin").permitAll();
 
         // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
         // If no login, it will redirect to /login page.
