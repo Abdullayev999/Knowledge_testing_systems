a4152ac1d6cb9027dc93e94e1b9bdc48fa6a46c8
Farid <fabdullayev902@gmail.com>
2021-11-03 18:53:26 +0400

    make it better

--- src/main/java/com/spring/SpringExam/controllers/RelaxController.java
+++ src/main/java/com/spring/SpringExam/controllers/RelaxController.java
@@ -1,23 +1,17 @@
 package com.spring.SpringExam.controllers;
 
 
-import com.spring.SpringExam.models.Category;
-import com.spring.SpringExam.repositories.ICategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class RelaxController {
 
     @RequestMapping("/paint")
-    public String paint( Model model, HttpServletRequest httpRequest) { return "paint"; }
+    public String paint( Model model, HttpServletRequest httpRequest) { return "relax/paint"; }
 
     @RequestMapping("/searchMovie")
     public String searchMovie( Model model, HttpServletRequest httpRequest) {
